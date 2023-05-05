8df3920840444e987c645eec4d848305475c595b
zxcvbaaa <130950930+zxcvbaaa@users.noreply.github.com>
2023-05-05 22:03:23 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,11 +8,15 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
-def ridge(data):
-    pass
+def ridge(x,y):
+     lam = 0.2
+    xTx=np.dot(x.T,x)
+    denom=xTx+np.eye(np.shape(x)[1])*lam
+    ws=denom.I*(x.T*y)
+    return ws
     
-def lasso(data):
-    pass
+#def lasso(data):
+   # pass
 
 def read_data(path='./data/exp02/'):#path data是上一级目录的
     x = np.load(path + 'X_train.npy')
@@ -21,19 +25,9 @@ def read_data(path='./data/exp02/'):#path data是上一级目录的
 
 def main(data):
     x,y=read_data()
-    weight=model(x.y)
+    weight=ridge(x.y)
     print(weight)
     return data @ weight
 
-def model(x, y):
-   def model(x, y):
-    # 基础的最小二乘实现线性回归
-    # $(X^TX)^-1(X^Ty)$
-    lam = 0.2
-    xTx=np.dot(x.T,x)
-    denom=xTx+np.eye(np.shape(x)[1])*lam
-    if np.linalg.det(denom)==0.0:
-        print("矩阵为奇异矩阵，不能转置")
-        return
-    ws=denom.I*(x.T*y)
-    return ws
+    
+   
