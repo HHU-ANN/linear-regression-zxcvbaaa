5a6e2d7d34ee31b237fa838eee6afaf5c02bc79a
zxcvbaaa <130950930+zxcvbaaa@users.noreply.github.com>
2023-05-06 10:09:31 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -72,6 +72,7 @@ def lasso(data):
     X_train, y_train = read_data()
     lasso_reg = Lasso( )
     w=lasso_reg.fit(x=X_train,y=y_train)
-    X_train = np.concatenate((np.ones((6,1)),X_train),axis=1)
+    m=X_train.shape[0]
+        X_train = np.concatenate((np.ones((m,1)),X_train ),axis=1)
     result = lasso_reg.predict(X_train,w) # 进行预测
     return float(result)
