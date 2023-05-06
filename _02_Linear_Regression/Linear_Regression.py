60010311a8a618a29227b552ae2bc4eddfbb1f4e
zxcvbaaa <130950930+zxcvbaaa@users.noreply.github.com>
2023-05-06 20:15:11 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -61,7 +61,7 @@ def lasso(data):
     X_train, y_train = read_data()
     
     learning_rate = 0.00000015  # 学习率
-    n_iterations = 20  # 迭代次数
+    n_iterations = 100  # 迭代次数
     
     theta = np.ones(6) 
 
